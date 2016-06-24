3991f19d48f277bf9a5cb6301c317f6284753d3d
Tim Wright <mithgirw@gmail.com>
2016-06-24 15:33:13 -0600

    adding support for multiple addresses

--- app/app.js
+++ app/app.js
@@ -1,21 +1,76 @@
-"use strict"
+"use strict";
 
 //an object constructor that creates first and last name key value pairs
-function Contact(firstname, lastname, addressline1, addressline2, city, stateregion, postalcode, country, telephone, emailaddress) {
+function Contact(firstname, lastname, telephone, emailaddress) {
     this.firstName = firstname;
     this.lastName = lastname;
+    this.addresses = [];
+    this.telephone = telephone;
+    this.emailAddress = emailaddress;
+}
+
+//an object constructor for an address
+function Address(addressline1, addressline2, city, stateregion, postalcode, country) {
     this.addressLine1 = addressline1;
     this.addressLine2 = addressline2;
     this.city = city;
     this.stateRegion = stateregion;
     this.postalCode = postalcode;
     this.country = country;
-    this.telephone = telephone;
-    this.emailAddress = emailaddress;
+}
+
+//a method that concactinates a full name
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
 }
 
 
 $(document).ready(function() {
+
+    $("#addanotheraddress").click(function(event){
+        event.preventDefault();
+        $("#addresses").append(
+            "<div id='new-addresses'>" + 
+            "<hr>" +
+                                "<div class='form-group'>" +
+                                    "<label class='control-label col-sm-2' for='addressline1'>Address:</label>" +
+                                    "<div class='col-sm-10'>" +
+                                        "<input type='text' class='form-control' id='addressline1'>" +
+                                    "</div>" +
+                                "</div>" +
+                                "<div class='form-group'>" +
+                                    "<label class='control-label col-sm-2' for='addressline2'></label>" +
+                                    "<div class='col-sm-10'>" +
+                                        "<input type='text' class='form-control' id='addressline2'>" +
+                                    "</div>" +
+                                "</div>" +
+                                "<div class='form-group'>" +
+                                    "<label class='control-label col-sm-2' for='city'>City:</label>" +
+                                    "<div class='col-sm-10'>" +
+                                        "<input type='text' class='form-control' id='city'>" +
+                                    "</div>" +
+                                "</div>" +
+                                "<div class='form-group'>" +
+                                    "<label class='control-label col-sm-2' for='stateregion'>State/Province/Region:</label>" +
+                                    "<div class='col-sm-10'>" +
+                                        "<input type='text' class='form-control' id='stateregion'>" +
+                                    "</div>" +
+                                "</div>" +
+                                "<div class='form-group'>" +
+                                    "<label class='control-label col-sm-2' for='postalcode'>ZIP/Postal Code:</label>" +
+                                    "<div class='col-sm-10'>" +
+                                        "<input type='text' class='form-control' id='postalcode'>" +
+                                    "</div>" +
+                                "</div>" +
+                                "<div class='form-group'>" +
+                                    "<label class='control-label col-sm-2' for='country'>Country:</label>" +
+                                    "<div class='col-sm-10'>" +
+                                        "<input type='text' class='form-control' id='country'>" +
+                                    "</div>" +
+                                "</div>"
+                                );
+    });
+
     //run this function on clicking the add contact button
     $("#addcontact").click(function(event){
         event.preventDefault();
@@ -35,16 +90,16 @@ $(document).ready(function() {
         if (submittedFirstName == "" || submittedLastName == "") {
             alert("Please enter a first and last name");
         }else {
-            //define a variable that runs the new contact object constructor with the arguments of first and last name passed in
+            //define a variable that runs the new contact object constructor with the arguments of the person's info passed in.
             var newContact = new Contact(submittedFirstName, submittedLastName, submittedAddressLine1, submittedAddressLine2, submittedCity, submittedStateRegion, submittedPostalCode, submittedCountry, submittedTelephone, submittedEmailAddress);
             
             //append the new contacts first and last names to the list
-            $("#contactslist").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>" );
+            $("#contactslist").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>" );
             console.log(newContact);
 
             $(".contact").last().click(function() {
                 $("#contactdetails").show();
-                $("#contactdetails h2").text(newContact.firstName + " " + newContact.lastName);
+                $("#contactdetails h2").text(newContact.fullName());
                 $("#detail-firstname").text(newContact.firstName);
                 $("#detail-lastname").text(newContact.lastName);
                 $("#detail-addressline1").text(newContact.addressLine1);
