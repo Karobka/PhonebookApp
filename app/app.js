82b15fa51a5ba6de86e0ef44b1b4900b1a3be074
Tim Wright <mithgirw@gmail.com>
2016-06-23 22:26:52 -0600

    posting user input to the list works

--- app/app.js
+++ app/app.js
@@ -1,4 +1,35 @@
+//an object constructor that creates first and last name key value pairs
+function Contact(firstname, lastname) {
+    this.firstName = firstname;
+    this.lastName = lastname;
+}
+
+
 $(document).ready(function() {
-    
-    
+    //fun this function on clicking the add contact button
+    $("#addcontact").click(function(event){
+        event.preventDefault();
+        
+        //define variables for the input values
+        var submittedFirstName = $("input#firstname").val();
+        var submittedLastName = $("input#lastname").val();
+        
+        //define a variable that creates a new contact with the arguments of first and last name passed in
+        var newContact = new Contact(submittedFirstName, submittedLastName);
+        
+        //append the new contacts first and last names to the list
+        $("#contactslist").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>" );
+
+        //clear form inputs
+        $("#firstname").val("");
+        $("#lastname").val("");
+        $("#addressline1").val("");
+        $("#addressline2").val("");
+        $("#city").val("");
+        $("#stateregion").val("");
+        $("#postalcode").val("");
+        $("#country").val("");
+        $("#telephone").val("");
+        $("#emailaddress").val("");
+    });
 });
